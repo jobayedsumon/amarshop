044291faa6004bd56b4b91b82ceaadd77ea6cfd6
jobayedsumon <jobayedahmedsumon@gmail.com>
2020-09-11 02:24:38 +0600

    Facebook login integrated

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://amarshop.com.bd/login/google/callback',
+        'redirect' => 'https://amarshop.com.bd/login/google/callback',
     ],
 
 ];
